53b802e912425e56ebf0cdb5433bd48323ad9a21
madeleine2626 <61909649+madeleine2626@users.noreply.github.com>
2020-03-26 17:41:30 +0200

    wrong import

--- FirstApp/src/main/java/com/apps/security/AuthenticationFilter.java
+++ FirstApp/src/main/java/com/apps/security/AuthenticationFilter.java
@@ -3,12 +3,12 @@ package com.apps.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import model.UserDetailsRequestModel;
 import model.UserLoginRequestModel;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -16,8 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -46,7 +45,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) throws IOException, ServletException {
 
-        String userName = ((UserDetailsRequestModel) auth.getPrincipal()).getUsername();
+        String userName = ((User) auth.getPrincipal()).getUsername();
         String token = Jwts.builder()
                 .setSubject(userName)
                 .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
