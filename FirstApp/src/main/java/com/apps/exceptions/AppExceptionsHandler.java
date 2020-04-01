12e4ddcfc055f30b52d942437f12bcbc94da84b3
madeleine2626 <61909649+madeleine2626@users.noreply.github.com>
2020-04-01 19:31:42 +0300

    handle all other exceptions - lection 63

--- FirstApp/src/main/java/com/apps/exceptions/AppExceptionsHandler.java
+++ FirstApp/src/main/java/com/apps/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,7 @@
 package com.apps.exceptions;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,11 +9,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import model.ErrorMessage;
+
 @ControllerAdvice
 public class AppExceptionsHandler {
 
 	@ExceptionHandler(value = {UserServiceException.class})
 	public ResponseEntity<Object> handleUserServiceException(UserServiceException exception, WebRequest request) {
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), exception.getMessage());
+		return new ResponseEntity<>(exception.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(value = {Exception.class})
+	public ResponseEntity<Object> handleOtherException(Exception exception, WebRequest request) {
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), exception.getMessage());
 		return new ResponseEntity<>(exception.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
